9b04580bb8c3e5978b91370c3c52f81a74815b11
Ednilson <ednilsoncastanheira53@gmail.com>
2026-02-12 14:17:34 +0000

    auth layer is done

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from .database import engine, Base
+from . import models
+from .routers import auth
+
+from .auth import get_current_user
+from .models import User
 
 app = FastAPI()
 
@@ -9,3 +14,13 @@ Base.metadata.create_all(bind=engine)
 @app.get("/")
 def read_root():
     return {"message": "Client & Infrastructure Manager API"}
+
+@app.get("/me")
+def read_me(current_user: User = Depends(get_current_user)):
+    return {
+        "id": current_user.id,
+        "email": current_user.email,
+        "role": current_user.role
+    }
+
+app.include_router(auth.router)
