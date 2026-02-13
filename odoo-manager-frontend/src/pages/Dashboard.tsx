05dea9ae3ee054a94321f3813972b8a586d6bdba
Ednilson <ednilsoncastanheira53@gmail.com>
2026-02-13 15:58:11 +0000

    now we can see real data in the dashboard

--- odoo-manager-frontend/src/pages/Dashboard.tsx
+++ odoo-manager-frontend/src/pages/Dashboard.tsx
@@ -1,19 +1,56 @@
-import { useAuth } from '../context/useAuth';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import api from "../api/axios";
+import type { Project } from "../types/project";
+import { useAuth } from "../context/useAuth";
 
-const Dashboard = () => {
+export default function Dashboard() {
   const { user, logout } = useAuth();
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const res = await api.get("/projects");
+        setProjects(res.data);
+      } catch (err) {
+        setError("Failed to load projects");
+        console.error(err);
+      }
+    };
+
+    fetchProjects();
+  }, []);
 
   return (
-    <div style={{ padding: '2rem' }}>
+    <div>
       <h1>Dashboard</h1>
+
       {user ? (
-        <p>Welcome back, <strong>{user.email}</strong>! (Role: {user.role})</p>
+      <p>
+        Welcome back, {user?.email}! (Role: {user?.role})
+      </p>
       ) : (
         <p>Loading user data...</p>
       )}
+      
+    
       <button onClick={logout}>Logout</button>
+
+      <hr />
+
+      <h2>Your Projects</h2>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      {projects.map((project) => (
+        <div key={project.id}>
+          <Link to={`/projects/${project.id}`}>
+            {project.name}
+          </Link>
+        </div>
+      ))}
     </div>
   );
-};
-
-export default Dashboard;
+}
