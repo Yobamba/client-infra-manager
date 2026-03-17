164954f436b19f18b421bf896578502f82104f62
Ednilson <ednilsoncastanheira53@gmail.com>
2026-03-17 19:18:20 +0000

    trying to force https instead of ws

--- app/database.py
+++ app/database.py
@@ -1,21 +1,25 @@
 import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.pool import StaticPool 
+from sqlalchemy.pool import StaticPool
+from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")  
+DATABASE_URL = os.getenv("DATABASE_URL")
 TURSO_AUTH_TOKEN = os.getenv("TURSO_AUTH_TOKEN")
 
+connection_url = DATABASE_URL
+if DATABASE_URL.startswith("https://"):
+    connection_url = DATABASE_URL.replace("https://", "sqlite+libsql://", 1)
+
 engine = create_engine(
-    DATABASE_URL,
+    connection_url,
     connect_args={
         "auth_token": TURSO_AUTH_TOKEN,
-        "check_same_thread": False
+        "check_same_thread": False,
     },
-    poolclass=StaticPool
+    poolclass=StaticPool,
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
