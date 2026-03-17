9e57565038b91dee25358a70b9c2320ab2f10ebb
Ednilson <ednilsoncastanheira53@gmail.com>
2026-03-17 13:56:23 +0000

    corrected the same import in a different file

--- odoo-manager-frontend/src/pages/ProjectDetails.tsx
+++ odoo-manager-frontend/src/pages/ProjectDetails.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import api from "../api/axios";
 import type { Instance, OdooInstanceType } from "../types/instance";
 import { AppLayout } from "../components/AppLayout";
-import ErrorDialog from "@/components/errorDialog";
+import ErrorDialog from "../components/ErrorDialog";
 import {
   Card,
   CardContent,
